802b8e86c17b127f9f612ee01e0e2b558967a07e
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-04-19 00:23:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(name="roller-skate")
+{
+    return `This Saturday, I want to ${name}!`;
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+function mondayWork(name="go to the office")
+{
+    return `This Monday, I will ${name}.`;
+}
+mondayWork("work from home")
+mondayWork()
+function wrapAdjective(operator="*") {
+    return function (value="special") {
+      return `You are ${operator}${value}${operator}!`;
+    };
+  }
+wrapAdjective("%")("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
